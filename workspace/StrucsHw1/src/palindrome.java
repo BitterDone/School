e8933dae56c6e85a9be1daf7a26906fcd8a80e3c
BitterDone <bitter.daniel@gmail.com>
2014-01-29 23:28:38 -0600

    Data Strucs H1-2 finished

--- workspace/StrucsHw1/src/palindrome.java
+++ workspace/StrucsHw1/src/palindrome.java
@@ -2,8 +2,10 @@
 public class palindrome {
 	
 	public static boolean palin = false;
+	public static boolean done = false;
 	public static String resPrefix = "Untouched result";
-	public static String resSubstring = "Untouched result";
+	public static String resSubstring = "^";
+	public static int currPalinLen = 0;
 
 	public static void main(String[] args) {
 		String test1 = "skyfall";
@@ -16,21 +18,21 @@ public class palindrome {
 		String break2 = "";
 		
 		String ans = "Untouched answer";
-		ans = longestPalindromeSubstring(test2);
+		ans = longestPalindromeSubstring(test4);
 		print("Result: "+ans);
 	}
 
 	public static String longestPalindromeSubstring(String s){
 		print("\nEnter longestPalindromePrefix");
-		//String res = "Untouched result";
 		boolean cont = false;
 		int index = 0;
 		char first = '\u0021';// !
 		char test = '\u0024';// $
 		
+		int endIndex = 0;
 		int len = 0;
 		len = s.length();
-		//print("Len: "+len);
+		endIndex = s.length()-1; 
 		
 		if(len == 0){
 			print("\nEmpty string");
@@ -43,116 +45,159 @@ public class palindrome {
 		}else if(len>1){
 			print("len= "+len);
 			first = s.charAt(0);
-			test = s.charAt(1);
+			test = s.charAt(endIndex);
 			print("First: "+first);
 			print("Test: "+test);
 			cont = true;
 		}
 		
 		if(cont){
-			print("Continuing...");
-			for(int i=1;i<len;i++){
+			print("Continuing. Test string: "+s);
+			print("end: "+s.charAt(endIndex));
+			for(int i=0;i<len;i++){
 				first = s.charAt(i);
 				
-				for(int j=1;j<len;j++){
+				for(int j=endIndex;j>=0;j--){
 					test = s.charAt(j);
-					
 					if( (first!=test)  ){
 						print("Bad First,Test: "+first+":"+test);
-						//Move to next test, j++;
-					}else if((first==test)  ){
+						//Move to next test, j--;
+					}else if((first==test)  && (!done)){
 						print("Good First,Test: "+first+":"+test);
-						
+						//String ss = s.substring(i, j);
 						index = i;
-						print("Palin: "+palin+". Calling check2: "
-								+s+" "+i+" "+j);
-						check2(s, i, j);
+						if(i<=j){
+							print("Palin: "+palin+". Calling check2: "
+									+s+" "+i+" "+j);
+							String cheque = check2(s, i, j);
 						
+						//Update the answer variable if a longer substring is found
+						if( (palin) && (cheque.length() > resSubstring.length()) ){
+							resSubstring = cheque;
+							print("Updated result with "+cheque+"\n");
+						}
+						}
+					}else{
+						print("dafuq");
 					}
 				}
 				
-				
-				
-				
-				
+			
+			//Move to next element from front -> back, i++;
 				
 			}//end for
 		}
-		
+		if(resSubstring.length()==1){
+			resSubstring = Character.toString(s.charAt(0));
+		}
 		return resSubstring;
 	}//end longestPalindromeSubstring
 	
-	public static boolean check2(String st, int start, int end){
-		print("\nEnter check1()");
+	public static String check2(String st, int start, int end){
+		String localResSub = "&";
+		print("\nEnter check2()");
 		boolean comp = true;
 		boolean problem = false;
 		int origIndex = 0;
-		int backIndex = 0;
+		int backIndex = 0; 
 		origIndex = end;
 		backIndex = end;
 		print("backIndex: "+backIndex);
 		char front = '\u0024';
-		front = st.charAt(0);
+		front = st.charAt(start);
 		char back = '\u0024';
 		back = st.charAt(end);
 		print("Front, back: "+front+":"+back);
 		
-		if((end%2)==0){
-			end = end/2;
-			print("Even end: "+end+"\n");
-		}else if((end%2)==1){
-			end = (end/2) + 1;
-			print("Odd piv: "+end+"\n");
-//left off here
-		}else{
-			print("Irregular pivot: " + end+"\n");
+		if(end<start){
+			problem=true;
 		}
 		
-		for(int i=0;i<=end;i++){
-			front=st.charAt(i);
-			if((front!=back) && comp){
-				print("Front != back: "+front+":"+back);
-				comp=false;
-				problem = true;
-				palin=false;
-			}else if((front==back) && comp){
-				print("Front==back: "+front+":"+back);
-				if(end>(i+1)){
-					backIndex=backIndex-1;
-					back=st.charAt(backIndex);
-					comp=true;
-				}else if(end==i){
-					print("pivot reached. piv==i: "+end+":"+i);
-					comp=false;
-					palin=true;
-				}else if(end==(i+1)){
-					print("\npivot reached. piv==i+1: "+end+":"+i);
-					print("Pivot char: "+st.charAt(end));
-					comp=false;
-					palin=true;
+		if(!done && !problem){//pointless if already done
+			if(end>start){
+				print("end>start");
+				if((end%2)==0){
+					end = end/2;
+					print("Even end: "+end+"\n");
+				}else if((end%2)==1){
+					end = (end/2) + 1;
+					print("Odd piv: "+end+"\n");
 				}else{
-					print("?");
+					print("Irregular pivot: " + end+"\n");
+					comp=false;
+					problem=true;
+					//Appropriate?
+					done=true;
 				}
+			}else if(end==start){
+				print("end=start");
+				
 			}else{
-				if(problem){
-					print("Something went wrong");
-				}else if(palin && !problem){
-					print("origIndex: "+origIndex);
-					resPrefix = st.substring(0, origIndex+1);
-					print("Finished! Palindrome :"
-							+resPrefix);
-					
+				print("idk");
+			}
+			
+			for(int i=start;i<=end;i++){
+				front=st.charAt(i);
+				if((front!=back) && comp && !done){
+					print("Front != back: "+front+"!="+back+" i:"+i+" end:"+end);
+					comp=false;
+					problem = true;
+					palin=false;
+				}else if((front==back) && comp && !done){
+					print("Front==back: "+front+":"+back);
+					if(end>(i+1)){
+						backIndex=backIndex-1;
+						back=st.charAt(backIndex);
+						comp=true;
+					}else if(end==i){
+						print("pivot reached. piv==i: "+end+":"+i);
+						if(start!=(origIndex+1)){
+							localResSub = st.substring(start, origIndex+1);
+						}else{
+							localResSub = Character.toString(st.charAt(start));
+						}
+						print("Finished! Palindrome :"
+								+localResSub);
+						comp=false;
+						palin=true;
+					}else if(end==(i+1)){
+						print("\npivot reached. piv==i+1: "+end+": "+i);
+						print("Pivot char: "+st.charAt(end));
+						if(start!=(origIndex+1)){
+							localResSub = st.substring(start, origIndex+1);
+						}else{
+							localResSub = Character.toString(st.charAt(start));
+						}
+						print("Finished! Palindrome :"
+								+localResSub);
+						comp=false;
+						palin=true;
+					}else{
+						print("\n?\n");
+					}
 				}else{
-					print("Unsure");
-				}
-				
-				//print("Front, back: "+front+" "+back);
-				//print("Front: "+cha(front));
-				
-			}//end if
-		}//end for
-		print("Palindrome: "+palin+"\n");
-		return palin;
+					print("");
+					if(problem){
+						print("Something went wrong");
+					}else if(palin && !problem){
+						print("origIndex: "+origIndex);
+						if(start!=(origIndex+1)){
+							localResSub = st.substring(start, origIndex+1);
+						}else{
+							localResSub = Character.toString(st.charAt(start));
+						}
+						print("Finished! Palindrome :"
+								+localResSub);
+						
+					}else{
+						print("\nUnsure\n");
+					}
+					
+				}//end if
+			}//end for
+		}//end if(!done)
+		print("Palindrome ("+palin+"): "+localResSub+"\n");
+		return localResSub;
 	}//end check2
 	
 	public static void print(String s){
